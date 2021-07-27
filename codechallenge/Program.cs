ced1808017b0e6909bf4856270d6447341a793af
Bryce Zimbelman <bryce.zimbelman@snhu.edu>
2021-07-27 10:43:51 -0500

    code challenge solution

---
+++ codechallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		List<int> elements = new List<int>();
+		int[] arr = new int[] {90, 21, 15, 12, 70, 66, 89, 7};
+		int x = 97;
+		for (int i = 0; i < arr.Length; i++) {
+			int sum = 0;
+			for (int j = i; j < arr.Length; j++) {
+				sum += arr[j]; 
+				if (sum == x) {
+					for (int k = i; k<= j; k++) {
+						Console.WriteLine(arr[k]);
+					}
+					return;
+				}
+			}
+		}
+		return;
+	}
+}
